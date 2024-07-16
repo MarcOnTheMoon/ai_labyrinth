b454b2f9d5ee303575b656b2097be93823b3ff53
Marc Hensel <121319337+MarcOnTheMoon@users.noreply.github.com>
2024-07-16 15:27:23 +0200

    Basic imaging

---
+++ src/device/Python/ImageAcquisition.py
@@ -0,0 +1,205 @@
+"""
+Acquire image input data for AI labyrinth.
+
+The data contains the original camera frame as well as the labyrinth area cut
+out (i.e., mapped to an image).
+
+@author: Marc Hensel
+@contact: http://www.haw-hamburg.de/marc-hensel
+
+@copyright: 2024, Marc Hensel
+@version: 2024.07.15
+@license: CC BY-NC-SA 4.0, see https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
+"""
+
+import cv2
+import numpy as np
+from Camera import Camera
+
+class ImageAcquisition():
+
+    # -------------------------------------------------------------------------
+    # Constructor
+    # -------------------------------------------------------------------------
+
+    def __init__(self, cameraID=1, width=3*274, height=3*228, isRotate180=True):
+        """
+        Connects to a camera and gets the first pair of images.
+        
+        The images are the camera frame and an image of the pinball playing
+        field cut from the frame. In this context, the mapping from the
+        camera frame to the labyrinth area is initialized.
+        
+        The physical labyrinth has a dimension of 274x228 mm, which is mapped
+        to 3*(274x228) pixels by default.
+
+        Parameters
+        ----------
+        cameraID : int, optional
+            Camera to connect to (Default: 1)
+            
+        width : int, optional
+            Width of the image containing the labyrinth, only. (Default: 3*274)
+            
+        height : int, optional
+            Height of the image containing the labyrinth, only. (Default: 3*228)
+            
+        isRotate180 : boolean, optional
+            Rotate camera frame 180° left, if True. (Default: True)
+
+        Returns
+        -------
+        None.
+
+        """
+        # Connect to camera
+        self.__camera = Camera(cameraID=cameraID)
+
+        # Set geometry of the image containing the pinball field
+        self.__field = {
+            'width': width,
+            'height': height }
+
+        # Set rotation
+        self.__isRotate180 = isRotate180
+
+        # Set initial region in camera frame regarded as pinball field
+        boarder = 25
+        self.__field['upper left'] = [boarder, boarder]
+        self.__field['upper right'] = [self.__camera.width - boarder, boarder]
+        self.__field['lower left'] = [boarder, self.__camera.height - boarder]
+        self.__field['lower right'] = [self.__camera.width - boarder, self.__camera.height - boarder]
+
+        # Calculate transformation to cut out pinball field
+        self.__updatePerpectiveTransform()
+
+        # Get first pair of images
+        self.frame, self.fieldImage = self.nextFrame()
+
+    # -------------------------------------------------------------------------
+    # Release resources
+    # -------------------------------------------------------------------------
+
+    def close(self):
+        """
+        Close camera.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.__camera.close()
+
+    # -------------------------------------------------------------------------
+    # Change corner of ROI in source frame
+    # -------------------------------------------------------------------------
+
+    def changeSourceCorner(self, x, y):
+        """
+        Sets a new coordinate to select the labyrinth area.
+        
+        Set a new coordinate in the camera frame corresponding to one of the
+        corners of the labyrinth. The method replaces the corner being closest
+        to the new point (x,y).
+
+        Parameters
+        ----------
+        x : int
+            x-coordinate of the new corner point.
+        y : int
+            y-coordinate of the new corner point.
+
+        Returns
+        -------
+        None.
+
+        """
+        labels = ['upper left', 'upper right', 'lower left', 'lower right']
+
+        # Determine closest corner to new coordinates
+        distances = [0., 0., 0., 0.]
+        for i in range(4):
+            corner = self.__field[labels[i]]
+            distances[i] = (corner[0] - x)**2 + (corner[1] - y)**2
+        minIndex = distances.index(min(distances))
+        
+        # Assign new coordinates to closest corner
+        self.__field[labels[minIndex]] = [x, y]
+        self.__updatePerpectiveTransform()
+
+    # -------------------------------------------------------------------------
+    # Perspective transform
+    # -------------------------------------------------------------------------
+
+    def __updatePerpectiveTransform(self):
+        """
+        Update the 3x3 matrix to map ROI in frame to image containing pinball
+        field.
+
+        Returns
+        -------
+        None.
+
+        """
+        # Corners of source ROI in camera frame
+        srcCorners = np.float32([
+            self.__field['upper left'],
+            self.__field['upper right'],
+            self.__field['lower left'],
+            self.__field['lower right']])
+        
+        # Corners of destination ROI (i.e., image containing pinball field)
+        dstCorners = np.float32([
+            [0, 0],
+            [self.__field['width'], 0],
+            [0, self.__field['height']],
+            [self.__field['width'], self.__field['height']]])
+        
+        # Calculate transformation matrix
+        self.__transformMatrix = cv2.getPerspectiveTransform(srcCorners, dstCorners)
+
+    # -------------------------------------------------------------------------
+    # Get next pair of images
+    # -------------------------------------------------------------------------
+    
+    def nextFrame(self, isDrawSourceROI = True):
+        """
+        Get the next camera frame and extracted labyrinth as image.
+
+        Parameters
+        ----------
+        isDrawSourceROI : boolean, optional
+            Draw ROI corresponding to labyrinth in frame, if true. (Default: True)
+
+        Returns
+        -------
+        numpy.ndarray
+            Camera frame (with labyrinth area marked, if isDrawSourceROI is True).
+        numpy.ndarray
+            Extracted labyrinth as image.
+
+        """
+        self.frame = self.__camera.nextFrame()
+        
+        # Apply rotation
+        if self.__isRotate180 == True:
+            self.frame = cv2.rotate(self.frame, cv2.ROTATE_180)
+        
+        # Get pinball area from camera frame
+        self.fieldImage = cv2.warpPerspective(
+            self.frame,
+            self.__transformMatrix,
+            (self.__field['width'], self.__field['height']))
+        
+        # Draw source ROI of pinball area in camera frame
+        if isDrawSourceROI:
+            points = np.array([
+                self.__field['upper left'],
+                self.__field['upper right'], 
+                self.__field['lower right'],
+                self.__field['lower left']],
+                np.int32)
+            cv2.polylines(self.frame, [points], True, (0,0,255))
+        
+        return self.frame, self.fieldImage
---
+++ src/device/Python/Imaging.py
@@ -0,0 +1,255 @@
+"""
+Image processing for AI labyrinth application.
+
+@author: Marc Hensel
+@contact: http://www.haw-hamburg.de/marc-hensel
+
+@copyright: 2024, Marc Hensel
+@version: 2024.07.16
+@license: CC BY-NC-SA 4.0, see https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
+"""
+
+import cv2
+import numpy as np
+from ImageAcquisition import ImageAcquisition
+
+class Imaging():
+    
+    # ----------------------------------------------------------------------
+    # Constructor and initialization
+    # ----------------------------------------------------------------------
+        
+    def __init__(self, cameraID):
+        """
+        Init image acquisition and image processing.
+
+        Parameters
+        ----------
+        cameraID : int
+            Camera to connect to. See documentation of class Camera.
+
+        Returns
+        -------
+        None.
+
+        """
+        # Init image source
+        self.acquisition = ImageAcquisition(cameraID=cameraID)
+
+        # Init processing units
+        self.threshold = 125
+        self.__initBlobDetector()
+        
+        # Get first images
+        self.frame, self.image = self.nextFrame()
+
+    # ----------------------------------------------------------------------
+        
+    def __initBlobDetector(self, minArea=100, maxArea=1_750, minCircularity=0.6, maxCircularity=1.0):
+        """
+        Init blob detector to detect moving ball in binary difference images.
+        
+        Estimation of the areas in pixel, with 1 mm = 3 pixel:
+            Area_ball = pi * (12.8 / 2)^2 mm^2 ~ 129 mm^2
+            Area_ball = 129 (3 * 3 * pixel) ~ 1,161 pixel
+            Area_center = pi * (5.0 / 2)^2 mm^2 ~ 20 mm^2
+            Area_center = 20 (3 * 3 * pixel) ~ 180 pixel
+
+        Parameters
+        ----------
+        minArea : int, optional
+            Minimum area of a blob. (Default: 150)
+        maxArea : int, optional
+            Maximum area of a blob. (Default: 700)
+        minCircularity : float, optional
+            Minimum roundness of a blob. (Default: 0.6)
+        maxCircularity : float, optional
+            Maximum roundness of a blob. (Default: 1.0)
+
+        Returns
+        -------
+        None.
+
+        """
+        params = cv2.SimpleBlobDetector_Params()
+        params.filterByColor = True
+        params.blobColor = 255
+        params.minArea = minArea
+        params.maxArea = maxArea
+        params.filterByCircularity = True
+        params.minCircularity = minCircularity
+        params.maxCircularity = maxCircularity
+        params.filterByConvexity = False
+        params.filterByInertia = False
+        self.__blobDetector = cv2.SimpleBlobDetector_create(params) 
+        
+    # ----------------------------------------------------------------------
+    # Release resources
+    # ----------------------------------------------------------------------
+
+    def close(self):
+        """
+        Release resources.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.acquisition.close()
+        
+    # ----------------------------------------------------------------------
+    # Get next frame
+    # ----------------------------------------------------------------------
+
+    def nextFrame(self):
+        """
+        Get next camera frame and image of extracted pinball area.
+
+        Returns
+        -------
+        numpy.ndarray
+            Camera frame.
+        numpy.ndarray
+            Image of extracted pinball area.
+
+        """
+        self.frame, self.image = self.acquisition.nextFrame()
+        return self.frame, self.image
+
+    # ----------------------------------------------------------------------
+    # Image processing and analysis
+    # ----------------------------------------------------------------------
+
+    def detectBall(self):
+        """
+        Process and analyze current image.
+        
+        Creates a binary image of positive differences and detects blobs.
+        If only one blob is detected, its center location and radius are
+        returned.
+
+        Returns
+        -------
+        diffImage : numpy.ndarray
+            Gray-valued image of positive temporal differences.
+        binImage : numpy.ndarray
+            Binary difference image with detected blobs marked in color.
+        ballCenter : [float, float]
+            Blob's center location, if only one blob is detected (or None).
+        ballRadius : float
+            Blob's radius, if only one blob is detected (or None).
+
+        """
+        grayImage = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        binImage = self.__thresholdAndDilate(grayImage)
+        binImage, ballCenter, ballRadius = self.__blobDetection(binImage)
+        
+        return binImage, ballCenter, ballRadius
+
+    # ----------------------------------------------------------------------
+
+    def __thresholdAndDilate(self, srcImage):
+        """
+        Apply threshold and dilate blobs.
+        
+        The structure element self.__dilateStructureElement is used for dilation.
+
+        Parameters
+        ----------
+        srcImage : numpy.ndarray
+            Gray-valued source to process.
+
+        Returns
+        -------
+        numpy.ndarray
+            Image after thresholding and morphological dilation.
+
+        """
+        # Create binary image
+        _, binImage = cv2.threshold(srcImage, self.threshold, 255, cv2.THRESH_BINARY_INV)
+
+        # Remove circles (thin lines)
+        h_x = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1)) 
+        h_y = cv2.getStructuringElement(cv2.MORPH_RECT, (1,5)) 
+        binImage = cv2.morphologyEx(binImage, cv2.MORPH_OPEN, h_x)
+        binImage = cv2.morphologyEx(binImage, cv2.MORPH_OPEN, h_y)
+
+        # Fill gaps
+        h_close = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15,15)) 
+        binImage = cv2.morphologyEx(binImage, cv2.MORPH_CLOSE, h_close)
+
+        return binImage
+
+    # ----------------------------------------------------------------------
+        
+    def __blobDetection(self, binImage):
+        """
+        Detect blobs in binary image.
+        
+        The method uses the detector self.__blobDetector. Detected blobs are
+        drawn into the binary image in color. If only one blob is detected,
+        the method returns its center location and radius.
+
+        Parameters
+        ----------
+        binImage : numpy.ndarray
+            Binary image to analyze.
+
+        Returns
+        -------
+        binImage : numpy.ndarray
+            Binary source image with detected blobs marked in color.
+        [float, float]
+            Blob's center location (or None).
+        float
+            Blob's radius (or None).
+
+        """
+        # Detect blobs
+        keypoints = self.__blobDetector.detect(binImage)
+
+        # Draw detected blobs in images
+        binImage = cv2.cvtColor(binImage, cv2.COLOR_GRAY2BGR)
+        binImage = cv2.drawKeypoints(binImage, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        
+        # Determine keypoint representing moving ball
+        # TODO Find and return ball (max. size?)
+        if len(keypoints) > 0:
+            maxKeypoint = keypoints[0]
+            for key in keypoints:
+                if key.size > maxKeypoint.size:
+                    maxKeypoint = key
+            return binImage, maxKeypoint.pt, (maxKeypoint.size + 1)/2
+        else:
+            return binImage, None, None
+
+
+    # ----------------------------------------------------------------------
+    # Drawing
+    # ----------------------------------------------------------------------
+        
+    def drawCircle(self, image, center, radius, color, thickness):
+        """
+        Draw a circle in an image.
+
+        Parameters
+        ----------
+        image : numpy.ndarray
+            Image to draw circle in.
+        center : float[2]
+            Circle's center location (x,y).
+        radius : float
+            Circle's radius.
+        color : int[3]
+            Color formatted as BGR.
+        thickness : int
+            Line thickness.
+
+        Returns
+        -------
+        numpy.ndarray
+            Image with circle drawn.
+
+        """
+        return cv2.circle(image, (int(center[0]), int(center[1])), int(radius), color, thickness)
